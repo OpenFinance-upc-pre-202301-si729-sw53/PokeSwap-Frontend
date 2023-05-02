e7f9ffd457ef42822b0c1cfda58b5f995260d25d
Jhosaim <86936147+jhosR@users.noreply.github.com>
2023-05-02 16:40:34 -0500

    register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  userData: User;
 
+  @ViewChild('userForm', { static: false })
+  userForm!: NgForm;
+
+  constructor(private service: UserService) {
+    this.userData = {} as User;
+  }
+
+  onSubmit() {
+    console.log(11)
+    console.log(this.userData)
+    this.userForm.resetForm();
+  }
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,6 +2,7 @@ export interface User {
     user_id:number,
     name:string,
     password:string,
+    email:string,
     password_hash:string,
     created_at:Date,
 }
